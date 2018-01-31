faa43c72122359d38fdcd40321a23f6fa5e5a22b
Michael Chapman <submariner100@gmail.com>
2018-01-31 18:42:56 +0000

    PhysicsBody

--- CrossyRoadsDemo/GameViewController.swift
+++ CrossyRoadsDemo/GameViewController.swift
@@ -14,6 +14,7 @@ class GameViewController: UIViewController {
 
 	var sceneView: SCNView!
 	var scene: SCNScene!
+	var cameraNode: SCNNode!
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +24,52 @@ class GameViewController: UIViewController {
 	
 	func setupView() {
 		sceneView = view as! SCNView
+		sceneView.showsStatistics = true
+		sceneView.allowsCameraControl = true
+		sceneView.autoenablesDefaultLighting = true
+		
+		
 		setupScene()
 	}
 	
 	func setupScene() {
 		scene = SCNScene()
 		sceneView.scene = scene
+		setupCamera()
 	}
+	
+	func setupCamera() {
+		cameraNode = SCNNode()
+		cameraNode.camera = SCNCamera()
+		cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
+		scene.rootNode.addChildNode(cameraNode)
+		addSquare()
+		//addSphere()
+	}
+	
+	func addSquare() {
+		let geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+		let squareNode = SCNNode(geometry: geometry)
+		squareNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+		scene.rootNode.addChildNode(squareNode)
+		
+		let force = SCNVector3(x: 0, y: 10.0, z: 0)
+		let position = SCNVector3(x: 0.05, y: 0.05, z: 0.05)
+		//squareNode.physicsBody?.applyForce(force, asImpulse: true)
+		squareNode.physicsBody?.applyForce(force, at: position, asImpulse: true)
+		
+		let torqueForce = SCNVector4(x: 0, y: 0, z: 1, w: 1)
+		squareNode.physicsBody?.applyTorque(torqueForce, asImpulse: true)
+		
+		
+	}
+	
+	func addSphere() {
+		let geometry = SCNSphere(radius: 1.0)
+		let sphereNode = SCNNode(geometry: geometry)
+		sphereNode.position = SCNVector3(x: 0, y: 1.5, z: 0)
+		scene.rootNode.addChildNode(sphereNode)
+		
+	}
+	
 }
